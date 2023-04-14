3c471e3be5d6c23fdaed2cb784b3f138625f010e
BrendaOti <eteteotimeyin@gmail.com>
2023-04-14 22:17:08 +0100

    takehome

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 document.getElementById('btn').addEventListener('click', function Perfect() { document.getElementById('demo').innerHTML = 'Brenda'; alert('Picture Perfect') })
 
+let myName ="Efe";
+console.log(myName)
+
+const x = 12;
+console.log(x)
+
+let myFirstnumber = 10;
+let mySecondnumber = 30;
+const myTotalnumber = myFirstnumber + mySecondnumber;
+console.log(myTotalnumber)
+
+let firstName = "Efe";
+let firstname = "Asa";
+const fullName = firstName +" "+ firstname;
+console.log(fullName)
+
+let firstProgrammer;
+firstProgrammer = "Efe";
+console.log(firstProgrammer)
+
+let animal = "dog";
